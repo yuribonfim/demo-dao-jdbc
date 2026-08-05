646dff9139d0e7fe28bd297b55831e8629b1e3b3
Yuri Bonfim <yuribonfim07@gmail.com>
2026-08-05 10:56:48 -0300

    Department entity class modification

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,4 +1,12 @@
 package application;
 
+import entities.Department;
+
 public class Program {
+
+    void main() {
+
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
+    }
 }
