704145b7ef393d1ab48b6f43e1b036ff4bf78b38
Yuri Bonfim <yuribonfim07@gmail.com>
2026-08-07 19:52:00 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,31 @@
+package model.dao.impl;
+
+import model.dao.SellerDao;
+
+public class SellerDaoJDBC implements SellerDao {
+
+    @Override
+    public void insert(model.entities.Seller obj) {
+
+    }
+
+    @Override
+    public void update(model.entities.Seller obj) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public model.entities.Seller findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public java.util.List<model.entities.Seller> findAll() {
+        return null;
+    }
+}
