0eb74ea29bd5d0a55b29286547bee45b9ecd43b2
Yuri Bonfim <yuribonfim07@gmail.com>
2026-08-08 17:00:14 -0300

    findById implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,8 +1,22 @@
 package model.dao.impl;
 
+import db.DB;
 import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class SellerDaoJDBC implements SellerDao {
+
+    private Connection conn;
+
+    public SellerDaoJDBC(Connection conn) {
+        this.conn = conn;
+    }
 
     @Override
     public void insert(model.entities.Seller obj) {
@@ -21,6 +35,35 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public model.entities.Seller findById(Integer id) {
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+            st = conn.prepareStatement(
+                    "SELECT seller.*,department.Name as DepName " +
+                            "FROM seller INNER JOIN department " +
+                            "ON seller.DepartmentId = department.Id " +
+                            "WHERE seller.Id = ?");
+            st.setInt(1, id);
+            rs = st.executeQuery();
+            if (rs.next()) {
+                Department dep = new Department();
+                dep.setId(rs.getInt("DepartmentId"));
+                dep.setName(rs.getString("DepName"));
+                Seller obj = new Seller();
+                obj.setId(rs.getInt("Id"));
+                obj.setName(rs.getString("Name"));
+                obj.setEmail(rs.getString("Email"));
+                obj.setBirthDate(rs.getTimestamp("BirthDate"));
+                obj.setBaseSalary(rs.getDouble("BaseSalary"));
+                obj.setDepartment(dep);
+                return obj;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
         return null;
     }
 
